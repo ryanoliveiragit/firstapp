3258e032d160667a1ade172af98ffdb1d0ada199
ryanoliveiragit <ryanoliveirabrito178@gmail.com>
2026-01-01 08:53:19 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Welcome from "./components/welcome";
+
+function App() {
+
+  return (
+    <main className="container">
+   <Welcome />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/welcome.tsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div className="text-red-500">
+      <h1>Welcome to KNZ Applicatiodn</h1>
+      <p>This is the welcome component.</p>
+    </div>
+  );
+}
